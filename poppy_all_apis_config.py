492b8c5cf2cee965da4d8b2118e543c4a2f83f76
Paul Glass <paul.glass@rackspace.com>
2015-02-12 16:17:35 -0600

    Update poppy_all_apis test
    * Group requests by name
    * Avoid exceptions on bad responses
    * Create a service only at tests start, not whenever a user spawns

--- poppy_all_apis_config.py
+++ poppy_all_apis_config.py
@@ -1,19 +1,19 @@
 # coding:utf-8
 host = "https://preview.cdn.api.rackspacecloud.com/v1.0"
-min_wait = 1000
-max_wait = 1000
+min_wait = 3 * 1000
+max_wait = 3 * 1000
 
 tenant_id = None
 token = None
 
-create_service_weight = 2
-update_service_weight = 2
-delete_service_weight = 1
-delete_asset_weight = 10
-delete_all_assets_weight = 10
-list_services_weight = 20
-get_service_weight = 10
-list_flavors_weight = 4
-get_flavors_weight = 4
+create_service_weight = 5
+update_service_weight = 5
+delete_service_weight = 5
+delete_asset_weight = 50
+delete_all_assets_weight = 50
+list_services_weight = 100
+get_service_weight = 50
+list_flavors_weight = 20
+get_flavors_weight = 20
 
 flavors = ['cdn', 'երանգ']
